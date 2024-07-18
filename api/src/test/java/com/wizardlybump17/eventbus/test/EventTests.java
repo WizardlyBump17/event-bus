aff908f295c2bda22d163483b1882d6d91de1c67
WizardlyBump17 <davicraft130@gmail.com>
2024-07-18 19:11:59 -0300

    testing the event cancellation system

--- api/src/test/java/com/wizardlybump17/eventbus/test/EventTests.java
+++ api/src/test/java/com/wizardlybump17/eventbus/test/EventTests.java
@@ -3,6 +3,7 @@ package com.wizardlybump17.eventbus.test;
 import com.wizardlybump17.eventbus.listener.EventListener;
 import com.wizardlybump17.eventbus.listener.ListenerPriority;
 import com.wizardlybump17.eventbus.manager.ListenerManager;
+import com.wizardlybump17.eventbus.test.event.CancellableChangeStringEvent;
 import com.wizardlybump17.eventbus.test.event.ChangeStringEvent;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -39,5 +40,25 @@ class EventTests {
         listenerManager.fireEvent(event);
 
         Assertions.assertEquals("Hello Beautiful World!", event.getString());
+    }
+
+    @Test
+    void testCancellableChangeStringEvent() {
+        ListenerManager listenerManager = new ListenerManager();
+        listenerManager.addListener(EventListener.of(
+                CancellableChangeStringEvent.class,
+                event -> {
+                    throw new UnsupportedOperationException();
+                },
+                ListenerPriority.NORMAL,
+                true
+        ));
+
+        CancellableChangeStringEvent event = new CancellableChangeStringEvent("Hello World!");
+        event.setCancelled(true);
+        listenerManager.fireEvent(event);
+
+        Assertions.assertEquals("Hello World!", event.getString());
+        Assertions.assertTrue(event.isCancelled());
     }
 }
