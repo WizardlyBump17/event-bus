1106d61887b216b5341e44d729d9907fbd384ee2
WizardlyBump17 <davicraft130@gmail.com>
2024-07-19 13:44:12 -0300

    testing the cancellation system

--- spigot/src/test/java/com/wizardlybump17/eventbus/test/paper/EventTests.java
+++ spigot/src/test/java/com/wizardlybump17/eventbus/test/paper/EventTests.java
@@ -1,13 +1,16 @@
 package com.wizardlybump17.eventbus.test.paper;
 
+import com.wizardlybump17.eventbus.annotation.Listener;
 import com.wizardlybump17.eventbus.manager.ListenerManager;
 import com.wizardlybump17.eventbus.paper.event.PaperEvent;
 import com.wizardlybump17.eventbus.paper.listener.PaperMethodEventListener;
 import com.wizardlybump17.eventbus.test.paper.event.ChangeStringEvent;
 import com.wizardlybump17.eventbus.test.paper.event.PaperChangeIntegerEvent;
 import com.wizardlybump17.eventbus.test.paper.event.PaperChangeStringEvent;
+import lombok.NonNull;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EventTests {
@@ -27,5 +30,25 @@ class EventTests {
         manager.fireEvent(new PaperEvent(paperEvent));
         manager.fireEvent(new PaperEvent(new PaperChangeIntegerEvent(10)));
         assertEquals("Paper", paperEvent.getString());
+    }
+
+    @Test
+    void testCancellation() {
+        ListenerManager manager = new ListenerManager();
+
+        manager.addListeners(PaperMethodEventListener.getListeners(new CancellationTest()));
+
+        PaperChangeStringEvent event = new PaperChangeStringEvent("Hello World");
+        event.setCancelled(true);
+
+        assertDoesNotThrow(() -> manager.fireEvent(new PaperEvent(event)));
+    }
+
+    public static class CancellationTest {
+
+        @Listener(ignoreCancelled = true)
+        public void onEvent(@NonNull PaperChangeStringEvent event) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
